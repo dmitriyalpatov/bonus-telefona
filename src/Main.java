dd89b01169e7d6b858e81d931d96d0c18b89cce0
dmitriyalpatov <dmitriyalpatov@mail.ru>
2022-05-25 02:59:10 +0300

    job

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+        public static void main(String[] args) {
+            int amaunt = 1200;
+
+            boolean positive = true;
+
+            int percent = 1;
+
+            if (positive) {
+                percent = 1;
+            } else {
+                int not_enough = 0;
+            }
+
+            int deposit = 1000;
+
+            int balans = 100;
+
+
+            int bonus = amaunt * percent / 100;
+
+            if (bonus > 1000)
+
+            System.out.println("Your wallet =" + balans);
+
+            System.out.println("Your bonus =" + bonus);
+
+        }
+}
